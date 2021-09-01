ac5ba52a4f649a7cdc98d32c98a1452aaf164c80
gayanii <89904986+gayanii@users.noreply.github.com>
2021-09-01 14:06:55 +0530

    Add files via upload

---
+++ pawbossAPI/pawbossAPI/DBContexts/PawBossContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using pawbossAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pawbossAPI.DBContexts
+{
+    public class PawBossContext : DbContext
+    {
+        public virtual DbSet<User> User { get; set; }
+        public virtual DbSet<Pet> Pet { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseMySql("server=127.0.0.1;port=3306;user=root;password=;database=PawBossDB", new MySqlServerVersion(new Version(10, 1, 29)))
+                .UseLoggerFactory(LoggerFactory.Create(b => b
+                    .AddConsole()
+                    .AddFilter(Level => Level >= LogLevel.Information)))
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+
+        }
+    }
+}
---
+++ pawbossAPI/pawbossAPI/Enum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pawbossAPI
+{
+    public class Enum
+    {
+        public enum AdoptedStatus
+        {
+            NotAddopted = 0,
+            Addopted = 1
+        }
+    }
+}
